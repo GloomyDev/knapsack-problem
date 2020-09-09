b7841ac44d42a8e725550b8d28a21947b095035e
Joaquin Romera <joakromera@gmail.com>
2020-09-08 22:39:33 -0300

    removing seed from generator, it will eventually be replaced by a random number generator anyway

--- generator.cpp
+++ generator.cpp
@@ -7,6 +7,7 @@
 #include <iterator>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
 #include <time.h>
 #include <tuple>
 #include <utility>
@@ -15,40 +16,42 @@
 #define Item std::pair<int, int>
 #define Items std::vector<Item>
 
-Items generateItemsList(int num_items, int max_weight, int profit, int seed) {
-    srand(seed);
+static Items generateItemsList(const unsigned num_items, const unsigned max_weight, const unsigned profit)
+{
     Items items;
     items.reserve(num_items);
-    for (int i = 0; i < num_items; ++i) {
+    for (unsigned i = 0; i < num_items; ++i) {
         items.emplace_back(
-            (rand() % max_weight) + 1,
-            (rand() % profit) + 1
+            (static_cast<unsigned>(rand()) % max_weight) + 1,
+            (static_cast<unsigned>(rand()) % profit) + 1
         );
     }
     return items;
 }
 
-void printVectorToFile(int num_items, int max_weight, Items items, std::ofstream &f) {
+static void printVectorToFile(const unsigned num_items, const unsigned max_weight, const Items items, std::ofstream &f)
+{
     f << num_items << " " << max_weight << std::endl;
-    for(int i = 0; i < num_items; ++i) {
+    for(unsigned i = 0; i < num_items; ++i)
+    {
         f << items[i].first << " " << items[i].second << std::endl;
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-    if (argc != 5)
+    if (argc != 4)
     {
         std::cerr << "Not enough parameters" << std::endl;
         return 1;
     }
 
-    int num_items = atoi(argv[1]);
-    int max_weight = atoi(argv[2]);
-    int profit = atoi(argv[3]);
-    int seed = atoi(argv[4]);
+    const unsigned num_items = static_cast<unsigned>(atoi(argv[1]));
+    const unsigned max_weight = static_cast<unsigned>(atoi(argv[2]));
+    const unsigned profit = static_cast<unsigned>(atoi(argv[3]));
 
-    std::vector<Item> items = generateItemsList(num_items, max_weight, profit, seed);
+    std::vector<Item> items = generateItemsList(num_items, max_weight, profit);
     std::string fileName = std::to_string(num_items) + "i_" + std::to_string(max_weight) + "w_" + std::to_string(profit) + "p.txt";
     std::ofstream file(fileName, std::fstream::out);
     printVectorToFile(num_items, max_weight, items, file);
