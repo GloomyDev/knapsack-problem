c17714fab0884690651ee03b497f5e7b535661f7
Joaquin Romera <joakromera@gmail.com>
2020-09-08 22:11:57 -0300

    Revert "fixing bug from previous refactor and improving generator"
    This reverts commit 4170ce42c8ae80b176eb2e1a80793d336ea0f56b.

--- generator.cpp
+++ generator.cpp
@@ -17,7 +17,9 @@
 
 Items items;
 
-Items generateItemsList(int num_items, int max_weight, int profit, int seed) {
+int seed;
+
+Items generateItemsList(int num_items, int max_weight, int profit) {
     srand(seed);
     Items items;
     items.reserve(num_items);
@@ -38,22 +40,23 @@ void printVectorToFile(int num_items, int max_weight, Items items, std::ofstream
 }
 
 int main(int argc, char *argv[]) {
-
-    if(argc != 5)
-    {
+    int num_items = 0;
+    int max_weight = 0; 
+    int profit = 0;
+    if(argc != 5) {
         std::cerr << "Not enough parameters" << std::endl;
-        return 1;
+    } else {
+        num_items = atoi(argv[1]);
+        max_weight = atoi(argv[2]);
+        profit = atoi(argv[3]);
+        seed = atoi(argv[4]);
     }
 
-    int num_items = atoi(argv[1]);
-    int max_weight = atoi(argv[2]);
-    int profit = atoi(argv[3]);
-    int seed = atoi(argv[4]);
-
-    std::vector<Item> items = generateItemsList(num_items, max_weight, profit, seed);
+    std::vector<Item> items = generateItemsList(num_items, max_weight, profit);
     std::string fileName = std::to_string(num_items) + "i_" + std::to_string(max_weight) + "w_" + std::to_string(profit) + "p.txt";
-    std::ofstream file(fileName, std::fstream::out);;
-    printVectorToFile(num_items, max_weight, items, file);
-    file.close();
+    std::ofstream f;
+    f.open(fileName, std::fstream::out);
+    printVectorToFile(num_items, max_weight, items, f);
+    f.close();
     return 0;
 }
